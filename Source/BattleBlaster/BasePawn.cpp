1d830577caefdd0b10efa047adf2eb0a3ad097a7
Avanish <avanishcodeswork@gmail.com>
2026-08-22 02:45:31 +0530

    projectile bug fixed

--- Source/BattleBlaster/BasePawn.cpp
+++ Source/BattleBlaster/BasePawn.cpp
@@ -6,21 +6,21 @@
 // Sets default values
 ABasePawn::ABasePawn()
 {
- 	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	// Set this pawn to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
 	CapsuleComp = CreateDefaultSubobject<UCapsuleComponent>(TEXT("CapsuleComp"));
 	SetRootComponent(CapsuleComp);
+
 	BaseMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BaseMesh"));
 	BaseMesh->SetupAttachment(CapsuleComp);
+
 	TurretMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("TurretMesh"));
 	TurretMesh->SetupAttachment(BaseMesh);
 
 	ProjectileSpawnPoint = CreateDefaultSubobject<USceneComponent>(TEXT("ProjectileSpawnPoint"));
 	ProjectileSpawnPoint->SetupAttachment(TurretMesh);
-
-
 }
-
 
 void ABasePawn::RotateTurret(FVector LookAtTarget)
 {
@@ -43,6 +43,5 @@ void ABasePawn::Fire()
 	FRotator SpawnRotation = ProjectileSpawnPoint->GetComponentRotation();
 
 	DrawDebugSphere(GetWorld(), SpawnLocation, 25.0f, 12, FColor::Red, false, 3.0f);
-	
-
 }
+
