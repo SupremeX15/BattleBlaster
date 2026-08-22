19aaedf8e38b558f793a90c079a0781d13889a91
Avanish <avanishcodeswork@gmail.com>
2026-08-22 23:40:27 +0530

    health component

--- Source/BattleBlaster/Projectile.h
+++ Source/BattleBlaster/Projectile.h
@@ -33,5 +33,7 @@ public:
 	UFUNCTION()
 	void OnHit(UPrimitiveComponent* HitComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Damage")
+	float Damage = 25.0f;
 
 };
