2df86ee7847ba8e27a3a2a793c28b07119a5582d
Avanish <avanishcodeswork@gmail.com>
2026-08-22 14:34:35 +0530

    tower projectle debug sphere added

--- Source/BattleBlaster/Tower.cpp
+++ Source/BattleBlaster/Tower.cpp
@@ -6,23 +6,36 @@
 void ATower::BeginPlay()
 {
 	Super::BeginPlay();
-
-
+	FTimerHandle FireTimerHandle;
+	GetWorldTimerManager().SetTimer(FireTimerHandle, this, &ATower::CheckFireCondition, FireRate, true);
 }
 
 void ATower::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
-	if (Tank)
+	if (IsInFireRange())
 	{
-		float DistanceToTank = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
-		if (DistanceToTank <= FireRange)
-		{
-			RotateTurret(Tank->GetActorLocation());
-		}
+		RotateTurret(Tank->GetActorLocation());
 	}
+}
 
+void ATower::CheckFireCondition()
+{
+		if (Tank && IsInFireRange())
+		{
+			Fire();
+		}
+}
 
+bool ATower::IsInFireRange()
+{
+		bool Result = false;
 
+		if (Tank)
+		{
+			float DistanceToTank = FVector::Dist(GetActorLocation(), Tank->GetActorLocation());
+			Result = (DistanceToTank <= FireRange);
+		}
 
+		return Result;
 }
--- Source/BattleBlaster/Tower.h
+++ Source/BattleBlaster/Tower.h
@@ -26,5 +26,12 @@ public:
 	ATank* Tank;
 	UPROPERTY(EditAnywhere)
 	float FireRange = 300.0f;
+	UPROPERTY(EditAnywhere)
+	float FireRate = 2.0f;
+
+	void CheckFireCondition();
+	bool IsInFireRange();
+
+
 
 };
